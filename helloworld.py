e90e843ee19a470309b97ca9d2c2a340014e159c
Quang <hodangquang1234@gmail.com>
2019-08-01 21:30:43 +0700

    1st_commit

---
+++ helloworld.py
@@ -0,0 +1,38 @@
+# print(5 - 3)
+# String
+# print("What's your name? ")
+# name = input('enter your name:')
+# print('hi')
+# print(name)
+# input co tac dung j
+# doi cho dong ̉4 dong 6
+# name dong  4 co phai name dong 6 ko?
+# hobby = input('enter your hobbie:')
+# print("your hobby is ",hobby)
+
+# name = input('enter your name:')
+# age = input('enter your age:')
+# print('my name is',name)
+# print('My age is',age)
+
+# print(10 + 10)
+# print(10 - 10)
+# print(10 * 10)
+# print(10 / 10)
+# print(5//2)
+
+
+# print(type(10))
+# print(type("10"))
+# print(type('10'))
+
+# print('1' + '0')
+# print(1+0)
+
+# data = input('enter something:')
+# print(type(data))
+
+# year = int(input('enter your age:'))
+# print(2019 - year)
+c = int(input('enter:'))
+print(c * 1.8)+ 32))
